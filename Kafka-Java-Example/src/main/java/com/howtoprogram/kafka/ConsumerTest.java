9b73f46e139d797abf340dd69b8a5d2ef9654aba
howtoprogram <binickname@gmail.com>
2016-05-02 23:28:01 +0700

    updated

--- Kafka-Java-Example/src/main/java/com/howtoprogram/kafka/ConsumerTest.java
+++ Kafka-Java-Example/src/main/java/com/howtoprogram/kafka/ConsumerTest.java
@@ -9,27 +9,27 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class ConsumerTest {
 
-	public static void main(String[] args) {
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "192.168.33.10:9092");
-		props.put("group.id", "group-1");
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("auto.offset.reset", "earliest");
-		props.put("session.timeout.ms", "30000");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "192.168.33.10:9092");
+    props.put("group.id", "group-1");
+    props.put("enable.auto.commit", "true");
+    props.put("auto.commit.interval.ms", "1000");
+    props.put("auto.offset.reset", "earliest");
+    props.put("session.timeout.ms", "30000");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
-		kafkaConsumer.subscribe(Arrays.asList("HelloKafkaTopic"));
-		while (true) {
-			ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
-			for (ConsumerRecord<String, String> record : records) {
-				System.out.printf("offset = %d, value = %s", record.offset(), record.value());
-				System.out.println();
-			}
-		}
+    KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+    kafkaConsumer.subscribe(Arrays.asList("HelloKafkaTopic"));
+    while (true) {
+      ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+      for (ConsumerRecord<String, String> record : records) {
+        System.out.printf("offset = %d, value = %s", record.offset(), record.value());
+        System.out.println();
+      }
+    }
 
-	}
+  }
 
 }
--- Kafka-Java-Example/src/main/java/com/howtoprogram/kafka/ProducerTest.java
+++ Kafka-Java-Example/src/main/java/com/howtoprogram/kafka/ProducerTest.java
@@ -8,31 +8,31 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class ProducerTest {
 
-	public static void main(String[] args) {
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "192.168.33.10:9092");
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		Producer<String, String> producer = null;
-		try {
-			producer = new KafkaProducer<>(props);
-			for (int i = 0; i < 100; i++) {
-				String msg = "Message " + i;
-				producer.send(new ProducerRecord<String, String>("HelloKafkaTopic", msg));
-				System.out.println("Sent:" + msg);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "192.168.33.10:9092");
+    props.put("acks", "all");
+    props.put("retries", 0);
+    props.put("batch.size", 16384);
+    props.put("linger.ms", 1);
+    props.put("buffer.memory", 33554432);
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    Producer<String, String> producer = null;
+    try {
+      producer = new KafkaProducer<>(props);
+      for (int i = 0; i < 100; i++) {
+        String msg = "Message " + i;
+        producer.send(new ProducerRecord<String, String>("HelloKafkaTopic", msg));
+        System.out.println("Sent:" + msg);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
 
-		} finally {
-			producer.close();
-		}
+    } finally {
+      producer.close();
+    }
 
-	}
+  }
 
 }
